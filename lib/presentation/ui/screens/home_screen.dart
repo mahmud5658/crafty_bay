937482413c4e2598368448af1c402a90e8fe4261
mahmud5658 <almahmuda5@gmail.com>
2024-10-29 00:57:45 +0600

    home page

--- lib/presentation/ui/screens/home_screen.dart
+++ lib/presentation/ui/screens/home_screen.dart
@@ -1,12 +1,12 @@
-import 'package:carousel_slider/carousel_slider.dart';
+
 import 'package:crafty_bay/presentation/utils/app_colors.dart';
 import 'package:crafty_bay/presentation/utils/assets_path.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-
 import '../widgets/app_bar_icon_button.dart';
 import '../widgets/banner_slider.dart';
 import '../widgets/search_text_field.dart';
+import '../widgets/section_header.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -16,6 +16,8 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+
+  late final TextTheme textTheme = Theme.of(context).textTheme;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -29,12 +31,49 @@ class _HomeScreenState extends State<HomeScreen> {
             const SizedBox(height: 16,),
             const BannerSlider(),
             const SizedBox(height: 16,),
+            _buildCategoriesSection()
           ],
         ),
       ),
     );
   }
 
+  Column _buildCategoriesSection() {
+    return Column(
+            children: [
+              SectionHeader(title: 'Categories',onTap: (){},),
+              const SizedBox(height: 8,),
+              SizedBox(
+                height: 140,
+                child: _buildCategoriesListView(),
+              ),
+            ],
+          );
+  }
+
+  Widget _buildCategoriesListView() {
+    return ListView.separated(
+                scrollDirection:Axis.horizontal,
+                  itemCount: 10,
+                  itemBuilder: (context,index){
+                    return Column(
+                      children: [
+                        Container(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: AppColors.themeColor.withOpacity(.1),
+                            borderRadius: BorderRadius.circular(10)
+                          ),
+                          child: Icon(Icons.computer,size: 48,color: AppColors.themeColor,),
+                        ),
+                        const SizedBox(height: 4,),
+                        Text('Computers',style: TextStyle(color: AppColors.themeColor),)
+                      ],
+                    );
+              },
+                separatorBuilder: (_,__)=> const SizedBox(width: 8,)
+              );
+  }
   AppBar buildAppBar() {
     return AppBar(
       backgroundColor: Colors.white,
@@ -49,7 +88,3 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
-
-
-
-
