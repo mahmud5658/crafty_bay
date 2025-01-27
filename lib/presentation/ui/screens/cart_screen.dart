588de93e4f9dbe4f333b9286902e8ef299ac11f7
mahmud5658 <almahmuda5@gmail.com>
2025-01-27 20:23:12 +0600

    review screen

--- lib/presentation/ui/screens/cart_screen.dart
+++ lib/presentation/ui/screens/cart_screen.dart
@@ -1,6 +1,8 @@
+import 'package:crafty_bay/presentation/ui/utils/assets_path.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../../state_holders/bottom_nav_controller.dart';
+import '../utils/app_colors.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -12,14 +14,62 @@ class CartScreen extends StatelessWidget {
         backToHome();
       },
       canPop: false,
-      child: const Scaffold(
-        body: Center(
-          child: Text("Cart Screen"),
-        ),
+      child: Scaffold(
+        body: Column(
+          children: [
+            Expanded(child: ListView.separated(itemBuilder: (context,index){
+              return Card(
+                elevation: 5,
+                child: Row(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(10),
+                      child: Image.asset(AssetsPath.dummyProduct,height: 100,width: 100,),
+                    ),
+
+                  ],
+                ),
+              );
+            }, separatorBuilder: (_,__)=> SizedBox(height: 8,), itemCount: 10)),
+            _buildPriceAndCheckout()
+          ],
+        )
       ),
     );
   }
 
+  Container _buildPriceAndCheckout() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: AppColors.themeColor.withOpacity(0.2),
+          borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(8), topLeft: Radius.circular(10))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Price"),
+              Text(
+                "\$100",
+                style: TextStyle(
+                    fontSize: 18,
+                    fontWeight: FontWeight.w600,
+                    color: AppColors.themeColor),
+              )
+            ],
+          ),
+          SizedBox(
+            width: 150,
+            child: ElevatedButton(
+                onPressed: () {}, child: const Text('Add to Cart')),
+          )
+        ],
+      ),
+    );
+  }
   void backToHome() {
     Get.find<BottomNavController>().backToHome();
   }
--- lib/presentation/ui/screens/review_screen.dart
+++ lib/presentation/ui/screens/review_screen.dart
@@ -1,10 +1,85 @@
-import 'package:flutter/cupertino.dart';
+ import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import '../utils/app_colors.dart';
 
 class ReviewScreen extends StatelessWidget {
   const ReviewScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        leading:  GestureDetector(
+          onTap: (){
+            Get.back();
+          },
+            child: const Icon(Icons.arrow_back_ios)),
+        title: const Text('Reviews'),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.separated(itemBuilder: (context,index){
+              return Padding(
+                padding: const EdgeInsets.all(1),
+                child: Card(
+                  elevation: 2,
+                  child: Column(
+                    children: [
+                      Row(
+                        children: [
+                          CircleAvatar(
+                            backgroundColor: Colors.grey.shade300,
+                            child: const Icon(Icons.person_outline),
+                          ),
+                          const SizedBox(width: 5,),
+                          const Text("Abdullah Al mahmud",style: TextStyle(fontSize: 18,fontWeight: FontWeight.w600),)
+                        ],
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.only(top: 10,left: 10,bottom: 10),
+                        child: Text("Simple is a scam. The application is full of mindless irrelevant questions.If they don’t refund my money immediately I’ll contact the Col Department of consumer fraud.",style: TextStyle(color: Colors.black54,fontSize: 16),),
+                      )
+                    ],
+                  ),
+                ),
+              );
+            }, separatorBuilder: (_,__)=>const SizedBox(height: 8,), itemCount: 10),
+          ),
+          _buildAddReview(),
+        ],
+      ),
+    );
+  }
+
+  Container _buildAddReview() {
+    return Container(
+      padding: const EdgeInsets.all(10),
+      decoration: BoxDecoration(
+          color: AppColors.themeColor.withOpacity(0.2),
+          borderRadius: const BorderRadius.only(
+              topRight: Radius.circular(8), topLeft: Radius.circular(10))),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          const Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text("Reviews (1000)",style: TextStyle(fontSize: 16,fontWeight: FontWeight.w500),),
+            ],
+          ),
+          FloatingActionButton(
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(50)
+            ),
+            backgroundColor: AppColors.themeColor,
+            onPressed: (){
+
+            
+          },child: Icon(Icons.add,color: Colors.white,),)
+        ],
+      ),
+    );
   }
 }
